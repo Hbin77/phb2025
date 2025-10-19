2fd96bc4acbdf1000c15af100bc5f96b7b43ceb8
Hbin77 <phb007298@gmail.com>
2025-10-19 20:59:44 +0900

    ch4 완료

--- c프로그래밍2/ch4.cpp
+++ c프로그래밍2/ch4.cpp
@@ -137,57 +137,57 @@
 // 5번
 #include <stdlib.h>  // atoi() 함수 사용을 위해 추가
 
-void find_key(int key, int *ptr, int size);
-void increase_array(int *ptr, int size);
-
-int main(int argc, char *argv[])  // 명령행 인자 받기
-{
-    int i, key, size;
-    int *list;
-
-    size = atoi(argv[1]);
-    list = (int *)malloc(sizeof(int) * size);
-
-    printf("%d개의 정수를 입력하세요:\n", size);
-    for(i = 0; i < size; i++)
-    {
-        printf("[%d] : ", i);
-        scanf("%d", &list[i]);
-    }
-
-    // 배열의 모든 원소를 1씩 증가
-    increase_array(list, size);
-
-    // 명령행 인자를 정수로 변환
-    key = atoi(argv[2]);
-    printf("입력된 키 값: %d\n", key);
-
-    // 키 값 찾기
-    find_key(key, list, size);
-    free(list);
-
-    printf("End\n");
-    return 0;
-}
-
-void increase_array(int *ptr, int size)
-{
-    int i;
-
-    for(i = 0; i < size; i++)
-    {
-        *(ptr + i) += 1;
-    }
-    return;
-}
-
-void find_key(int key, int *ptr, int size)
-{
-    int i;
-
-    for(i = 0; i < size; i++)
-    {
-        if(key == *(ptr + i))
-            printf("Find key value : %d\n", i+1);
-    }
-}
+// void find_key(int key, int *ptr, int size);
+// void increase_array(int *ptr, int size);
+//
+// int main(int argc, char *argv[])  // 명령행 인자 받기
+// {
+//     int i, key, size;
+//     int *list;
+//
+//     size = atoi(argv[1]);
+//     list = (int *)malloc(sizeof(int) * size);
+//
+//     printf("%d개의 정수를 입력하세요:\n", size);
+//     for(i = 0; i < size; i++)
+//     {
+//         printf("[%d] : ", i);
+//         scanf("%d", &list[i]);
+//     }
+//
+//     // 배열의 모든 원소를 1씩 증가
+//     increase_array(list, size);
+//
+//     // 명령행 인자를 정수로 변환
+//     key = atoi(argv[2]);
+//     printf("입력된 키 값: %d\n", key);
+//
+//     // 키 값 찾기
+//     find_key(key, list, size);
+//     free(list);
+//
+//     printf("End\n");
+//     return 0;
+// }
+//
+// void increase_array(int *ptr, int size)
+// {
+//     int i;
+//
+//     for(i = 0; i < size; i++)
+//     {
+//         *(ptr + i) += 1;
+//     }
+//     return;
+// }
+//
+// void find_key(int key, int *ptr, int size)
+// {
+//     int i;
+//
+//     for(i = 0; i < size; i++)
+//     {
+//         if(key == *(ptr + i))
+//             printf("Find key value : %d\n", i+1);
+//     }
+// }
